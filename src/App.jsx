87455f36d0e03dff2a4ecd3900306e7cf0fb0b41
dongyuanwai <yuanwaidong@gmail.com>
2022-12-01 20:58:25 +0800

    feat: useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import useApi from './useApi'
+import React, { useEffect, useState, useMemo } from 'react'
+
+function Child({data}){
+  useEffect(()=>{
+    console.log("查询条件",data)
+  },[data])
+  return <div>子组件</div>
+}
 
 function App() {
-  const [{data},setQuery] = useApi()
+  const [name,setName] = useState("")
+  const [phone,setPhone] = useState("")
+  const [kw,setKw] = useState("")
 
+  const data = useMemo(()=>({
+    name,
+    phone
+  }),[name,phone]) 
   return (
     <div className="App">
-      {
-        data.map((item, index) => <span key={index}>{item}</span>)
-      }
-      <input onChange={(e) => setQuery(e.target.value)} type="text" placeholder='请输入搜索值' />
+      <input onChange={(e) => setName(e.target.value)} type="text" placeholder='请输入姓名' />
+      <input onChange={(e) => setPhone(e.target.value)} type="text" placeholder='请输入电话' />
+      <input onChange={(e) => setKw(e.target.value)} type="text" placeholder='请输入关键词' />
+      <Child data={data}></Child>
     </div>
   )
 }
