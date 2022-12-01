b06a4b29afd24e4ec432871c5797b57d0d8c8930
dongyuanwai <yuanwaidong@gmail.com>
2022-12-01 22:41:52 +0800

    feat: 引入zarmUI,配置按需加载

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import {
   Route
 } from "react-router-dom"
 import routes from '../src/router'
+
+import {ConfigProvider} from "zarm"
+import zhCN from 'zarm/lib/config-provider/locale/zh_CN'
+
 function App() {
   return <Router>
-     <Routes>
-      {routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component />} />)}
-     </Routes>
+    <ConfigProvider primaryColor={'#007fff'} locale={zhCN}>
+      <Routes>
+        {routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component />} />)}
+      </Routes>
+    </ConfigProvider>
    </Router>
 }
 
