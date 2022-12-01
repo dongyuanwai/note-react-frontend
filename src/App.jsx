6d09a7095e54ce59c2cc6ed3cacfc98c6961dbe7
dongyuanwai <yuanwaidong@gmail.com>
2022-12-01 22:07:04 +0800

    feat: 引入路由，创建路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import React, { useEffect, useState, useMemo } from 'react'
-
+import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import routes from '../src/router'
 function App() {
-  const [count,setCount] = useState(0)
-  const handleClick = ()=>{
-    setTimeout(()=>{
-      console.log("这是count的值"+count)
-    },3000)
-  }
-  return (
-    <div className="App">
-      <button onClick={()=>setCount(count+1)}>点击{count}此</button>
-      <button onClick={handleClick}>展示点击次数</button>
-      <h1>开始项目</h1>
-    </div>
-  )
+  return <Router>
+     <Routes>
+      {routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component />} />)}
+     </Routes>
+   </Router>
 }
 
 export default App
