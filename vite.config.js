b799431fd5e1c1a8691b6b5ed63b13ae405d2f52
dongyuanwai <yuanwaidong@gmail.com>
2022-12-02 21:58:37 +0800

    feat: 封装axios和配置别名

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { createStyleImportPlugin } from 'vite-plugin-style-import';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(),createStyleImportPlugin(
@@ -16,6 +17,7 @@ export default defineConfig({
       ]
     }
   )],
+  // 配置less
   css: {
     modules: {
       localsConvention: 'dashesOnly'
@@ -27,4 +29,24 @@ export default defineConfig({
       }
     }
   },
+  // 配置代理
+  server: {
+    proxy: {
+      '/api':{
+        // 当遇到 /api 路径时 换成target的值
+        target: 'http://api.chennick.wang/api/',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/,'')
+      },
+    }
+  },
+
+  // 配置别名
+  resolve: {
+    alias: {
+      '@':path.resolve(__dirname,'src'), //src路径
+      'utils': path.resolve(__dirname,'src/utils'),
+    }
+  },
+
 })
