5e1619ab4bbd4385ebb9c5c73d711753a9c52714
dongyuanwai <yuanwaidong@gmail.com>
2022-12-04 16:22:03 +0800

    feat: 注册页UI

--- src/container/Login/index.jsx
+++ src/container/Login/index.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React from 'react'
+import { Cell, Input, Button, Checkbox } from 'zarm'
+import CustomIcon from '@/components/CustomIcon'
 
 import s from './style.module.less'
 
-const Login = ()=>{
-    return <div className={s.auth}>注册</div>
+const Login = () => {
+  return <div className={s.auth}>
+    <div className={s.head} />
+    <div className={s.tab}>
+      <span>注册</span>
+    </div>
+    <div className={s.form}>
+      <Cell icon={<CustomIcon type="zhanghao" />}>
+        <Input
+          clearable
+          type="text"
+          placeholder="请输入账号"
+        />
+      </Cell>
+      <Cell icon={<CustomIcon type="mima" />}>
+        <Input
+          clearable
+          type="password"
+          placeholder="请输入密码"
+        />
+      </Cell>
+      <Cell icon={<CustomIcon type="mima" />}>
+          <Input
+            clearable
+            type="text"
+            placeholder="请输入验证码"
+          />
+        </Cell>
+    </div>
+    <div className={s.operation}>
+      <div className={s.agree}>
+        <Checkbox />
+        <label className="text-light">阅读并同意<a>《便捷记账条款》</a></label>
+      </div>
+      <Button block theme="primary">注册</Button>
+    </div>
+  </div>
 }
 
 export default Login
