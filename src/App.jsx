73d2ebb6a5d48ce039db7281725dbb50cd0a2e96
dongyuanwai <yuanwaidong@gmail.com>
2022-12-04 14:58:59 +0800

    feat: 导航栏&路由

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,33 @@ import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  useLocation
 } from "react-router-dom"
 import routes from '../src/router'
 
 import {ConfigProvider} from "zarm"
 import zhCN from 'zarm/lib/config-provider/locale/zh_CN'
 
+import NavBar from '@/container/Nav'
+import { useEffect } from 'react'
+
 function App() {
-  return <Router>
+  const location = useLocation()
+  const {pathname} = location
+  const needNav = ['/','/data','/user']
+  const [showNav,setShowNav] = useState(false)
+  useEffect(()=>{
+    setShowNav(needNav.includes(pathname))
+  },[pathname])
+  return <>
     <ConfigProvider primaryColor={'#007fff'} locale={zhCN}>
       <Routes>
         {routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component />} />)}
       </Routes>
     </ConfigProvider>
-   </Router>
+    <NavBar showNav={showNav} />
+   </>
 }
 
 export default App
