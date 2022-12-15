77c9b968f6a77c211369b981fd694c199e1a4086
dongyuanwai <yuanwaidong@gmail.com>
2022-12-15 20:13:01 +0800

    fix: 注册

--- src/container/Login/index.jsx
+++ src/container/Login/index.jsx
@@ -1,7 +1,8 @@
 import React,{useState,useCallback  } from 'react'
-import { Cell, Input, Button, Checkbox } from 'zarm'
+import { Cell, Input, Button, Checkbox, Toast } from 'zarm'
 import CustomIcon from '@/components/CustomIcon'
 import Captcha from "react-captcha-code"
+import { post } from '@/utils'
 
 import s from './style.module.less'
 
@@ -17,6 +18,32 @@ const Login = () => {
     console.log('captcha', captcha)
     setCaptcha(captcha)
   }, []);
+
+  const onSubmit = async () => {
+    console.log("注册信息",username,password,verify)
+    if (!username) {
+      Toast.show('请输入账号')
+      return
+    }
+    if (!password) {
+      Toast.show('请输入密码')
+      return
+    }
+    if (!verify) {
+      Toast.show('请输入验证码')
+      return
+    };
+    if (verify != captcha) {
+      Toast.show('验证码错误')
+      return
+    };
+    post('/api/user/register', {
+      username,
+      password
+    }).then(res => {
+      // do something
+    })
+  };
 
   return <div className={s.auth}>
     <div className={s.head} />
@@ -37,7 +64,7 @@ const Login = () => {
           clearable
           type="password"
           placeholder="请输入密码"
-          onChange={(value) => setUsername(value)}
+          onChange={(value) => setPassword(value)}
         />
       </Cell>
       <Cell icon={<CustomIcon type="mima" />}>
@@ -55,7 +82,7 @@ const Login = () => {
         <Checkbox />
         <label className="text-light">阅读并同意<a>《便捷记账条款》</a></label>
       </div>
-      <Button block theme="primary">注册</Button>
+      <Button onClick={onSubmit} block theme="primary">注册</Button>
     </div>
   </div>
 }
