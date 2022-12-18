5c2395afbc8aac0cf9d5aa24c2a7d7f661db8490
dongyuanwai <yuanwaidong@gmail.com>
2022-12-18 23:10:02 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         host: '43.143.134.60',
         ref: 'origin/master',
         repo: 'git@github.com:dongyuanwai/note-react-frontend.git',
-        path: '/note-react-frontend',
+        path: '/workspace/note-react-frontend',
         'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
         env: {
           NODE_ENV: 'production'
