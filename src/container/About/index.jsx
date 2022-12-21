ec001f88e3d3e882a8af45db697ace37c6eea4b9
dongyuanwai <yuanwaidong@gmail.com>
2022-12-21 22:02:58 +0800

    feat: 媒体查询

--- src/container/About/index.jsx
+++ src/container/About/index.jsx
@@ -14,14 +14,11 @@ const About = () => {
       <article>后端使用node+egg.js,数据库使用MySQL和可视化的DBeaver工具进行开发</article>
 
       <h2>关于我</h2>
-      <article>
-        <p>本人21年本科毕业，专业是计算机科学与技术</p>
+        <article>本人21年本科毕业，专业是计算机科学与技术</article>
 
-        <p>从事前端开发工作，现已工作两年有余，技术栈使用 Vue/React/node/JS/TS等。喜欢折腾，热爱分享</p>
+        <article>从事前端开发工作，现已工作两年有余，技术栈使用 Vue/React/node/JS/TS等。喜欢折腾，热爱分享</article>
 
         <a href="https://juejin.cn/user/3966693685594072">这是我的掘金首页</a>
-        <p></p>
-      </article>
     </div>
   </>
 };
